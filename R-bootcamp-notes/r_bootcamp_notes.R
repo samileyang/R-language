cac00ce337ae62eb6e9b12f6a386488b3ef8bf3d
Yihui Yang <35894512+samileyang@users.noreply.github.com>
2019-08-23 20:52:38 -0400

    Add files via upload

--- R-bootcamp-notes/r_bootcamp_notes.R
+++ R-bootcamp-notes/r_bootcamp_notes.R
@@ -24,3 +24,64 @@ head(year_country_gdp)
 
 year_country_gdp <- gapminder %>% select(year,country,gdpPercap)
 head(year_country_gdp)
+
+year_country_gdp_africa <- gapminder %>% 
+  filter(continent == "Africa") %>%
+  select(year,country,gdpPercap)
+
+#split-apply-combine
+gapminder$mean.contient.GPD <- NULL
+
+gdp_bycontinents <- gapminder %>%
+  group_by(continent) %>%
+  summarize(mean_gdpPercap = mean(gdpPercap))
+head(gdp_bycontinents)
+
+gdp_bycontinents_byyear <- gapminder %>%
+  group_by(continent,year) %>%
+  summarize(mean_gdpPercap = mean(gdpPercap))
+show(gdp_bycontinents_byyear)
+
+gdp_pop_bycontinents_byyear <- gapminder %>%
+  group_by(continent,year) %>%
+  summarize(mean_gdpPercap = mean(gdpPercap),
+            sd_gdpPercap = sd(gdpPercap),
+            mean_pop = mean(pop),
+            sd_pop = sd(pop))
+head(gdp_pop_bycontinents_byyear)
+
+gdp_pop_bycontinents_byyear_2 <- gapminder %>%
+  group_by(continent,year) %>%
+  summarise(mean_gdpPercap = mean(gdpPercap),
+            sd_gdpPercap = sd(gdpPercap),
+            mean_pop = mean(pop),
+            sd_pop = sd(pop))
+head(gdp_pop_bycontinents_byyear)
+
+gapminder_with_extra_vars <- gapminder %>%
+  group_by(continent, year) %>%
+  mutate(mean_gdpPercap = mean(gdpPercap),
+         sd_gdpPercap = sd(gdpPercap),
+         mean_pop = mean(pop),
+         sd_pop = sd(pop))
+head(gapminder_with_extra_vars)
+
+gdp_pop_bycontinents_byyear <- gapminder %>%
+  mutate(gdp_billion = gdpPercap*pop/10^9) %>%
+  group_by(continent, year) %>%
+  summarize(mean_gdpPercap = mean(gdpPercap),
+            sd_gdpPercap = sd(gdpPercap),
+            mean_pop = mean(pop),
+            sd_pop = sd(pop),
+            mean_gdp_billion = mean(gdp_billion),
+            sd_gdp_billion = sd(gdp_billion))
+head(gdp_pop_bycontinents_byyear)
+
+gapminder_with_extra_vars <- gapminder %>%
+  group_by(continent, year) %>%
+  mutate(mean_gdpPercap = mean(gdpPercap),
+         sd_gdpPercap = sd(gdpPercap),
+         mean_pop = mean(pop),
+         sd_pop = sd(pop)) %>%
+  arrange(desc(year), continent)
+head(gapminder_with_extra_vars)
